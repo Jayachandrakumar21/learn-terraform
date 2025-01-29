f141e33faf8cc9460d514e98717de5f5798c0aea
jayachandra_kumar <jayachandrakumar21@gmail.com>
2025-01-29 05:57:09 +0530

    learn-terraform

---
+++ ec2.tf
@@ -0,0 +1,14 @@
+# resource "aws_instance" "this" {
+#   ami                     = "ami-09c813fb71547fc4f"
+#   instance_type           = "t2.micro"
+  
+# }
+
+resource "aws_security_group" "allow_tls" {
+    name = "allow_tls"
+    description = "Allow TLS inbount traffic and outbound traffic"
+
+    tags = {
+        name + "allow_tls"
+    }
+}
