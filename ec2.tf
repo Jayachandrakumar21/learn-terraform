0302fcd3e32b04c5afbcd90388d7fc3942ee5c88
jayachandra_kumar <jayachandrakumar21@gmail.com>
2025-06-29 14:35:20 +0530

    working with git

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,10 @@
-<<<<<<< HEAD
+
+# Created by: Jayachandra_kumar Date:28-01-2025
+# Topic: Terraform ec2 instance creation 
+
 resource "aws_instance" "this" {
   ami                     = "ami-09c813fb71547fc4f"
   instance_type           = "t2.micro"
-=======
-# Created by: Jayachandra_kumar Date:28-01-2025
-# Topic: Terraform ec2 instance creation 
-# resource "aws_instance" "this" {
-#   ami                     = "ami-09c813fb71547fc4f"
-#   instance_type           = "t2.micro"
->>>>>>> e1e3d3b3aa36f7063a1c28b05c89e00d197ac37c
-  
 }
 
 resource "aws_security_group" "allow_tls" {
